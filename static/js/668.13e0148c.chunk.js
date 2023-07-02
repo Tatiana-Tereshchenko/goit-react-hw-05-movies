da2d7f14c6cfe156a5454d4a01e4e9989237dd9a
Tatiana-Tereshchenko <117577287+Tatiana-Tereshchenko@users.noreply.github.com>
2023-07-02 20:10:28 +0000

    Deploying to gh-pages from @ Tatiana-Tereshchenko/goit-react-hw-05-movies@07dcfbce734c873843c3c275a6a3977a4ab9de59 🚀

---
+++ static/js/668.13e0148c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[668],{668:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(87),o="NotFound_box__DiuDZ",u=r(184);var a=function(){return(0,u.jsx)("div",{className:o,children:(0,u.jsx)(n.rU,{to:"/",children:"Not Found page"})})}}}]);
+//# sourceMappingURL=668.13e0148c.chunk.js.map
--- static/js/668.c58d1baf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[668],{668:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r="NotFound_box__DiuDZ",u=n(184);var a=function(){return(0,u.jsx)("div",{className:r,children:"Not Found page"})}}}]);
-//# sourceMappingURL=668.c58d1baf.chunk.js.map
